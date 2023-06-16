150b1b338bf4da2d031dc5185b6e999345718b49
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-06-16 09:44:38 -0300

    main
    completando logica js del to-do

--- experiencias/to-do/js/main.js
+++ experiencias/to-do/js/main.js
@@ -1,3 +1,5 @@
+const todos = [];
+
 window.onload = () =>{
     const formulario = document.getElementById("todo-form");
     formulario.onsubmit = (e) => {
@@ -6,5 +8,15 @@ window.onload = () =>{
         const todoText = todo.value;
         todo.value = '';
         console.log(todoText);
+        todos.push(todoText);
+
+        const todoList = document.getElementById("todo-list");
+        const todoTemplate = todos.map(t => '<li>'+t+'</li>' );
+        todoList.innerHTML = todoTemplate.join('');
+
+        // todoList.innerHTML= '';
+        // for(let i=0; i < todos.length;i++){
+        //     todoList.innerHTML += '<li>'+todos[i]+'</li>';
+        // }
     }
 }
