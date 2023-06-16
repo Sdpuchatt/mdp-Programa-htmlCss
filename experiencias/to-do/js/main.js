cf6a56e74cfc499da95754d46646a3e31b705ae6
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-06-16 09:18:44 -0300

    main
    creando app todo-list en experiencias

---
+++ experiencias/to-do/js/main.js
@@ -0,0 +1,10 @@
+window.onload = () =>{
+    const formulario = document.getElementById("todo-form");
+    formulario.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById("todo");
+        const todoText = todo.value;
+        todo.value = '';
+        console.log(todoText);
+    }
+}
