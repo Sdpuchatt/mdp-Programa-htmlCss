504b91d626d728142307b9a4e93b9e7c462afdce
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-08-02 16:07:27 -0300

    main
    ejercicio del lunes primero de agosto array de objetos persona

---
+++ modulo JS/practica 03 29-7/objetoPersona.js
@@ -0,0 +1,52 @@
+const prompt = require("prompt-sync")({sigint:true});
+//npm install prompt-sync
+
+let dataPerson = [];
+
+const addDataPerson = (fistName,lastName,birthDay,status) =>{
+    person = {
+        fistName,
+        lastName,
+        birthDay,
+        status
+    }
+    dataPerson.push(person);
+}
+
+addDataPerson('jose','rivera',33,0);
+addDataPerson('ariel','edmundo',25,0);
+addDataPerson('pascuala','lavaca',20,0);
+addDataPerson('ismael','hernandez',15,0);
+addDataPerson('astor','piazolla',17,0);
+addDataPerson('elOfe','edmundo',18,0);
+addDataPerson('carlos','gardel',21,0);
+addDataPerson('mariano','martinez',40,0);
+addDataPerson('mercedes','sosa',65,0);
+addDataPerson('violeta','parra',70,0);
+
+console.table(dataPerson);
+
+// dataPerson.forEach((element) => {
+//     console.log(element.fistName);
+// });
+const forDataPerson = (elemento, indice, ) =>{
+    //comilla invertida alt + 96; 
+    console.log(`nombre: ${elemento.fistName} posicion: ${indice}`)
+}
+dataPerson.forEach(forDataPerson);
+
+let nuevoDataPerson = dataPerson.filter((element)=>{
+    return element.fistName.includes("as");
+});
+
+console.table(nuevoDataPerson);
+
+promedioEdad = () => {
+    let edadPromedio = 0;
+    for(let i=0; i<dataPerson.length;i++){
+        edadPromedio += dataPerson[i].birthDay;
+    }
+    edadPromedio = edadPromedio/dataPerson.length;
+    console.log(`El promedio de edades es: ${edadPromedio}`);
+}
+promedioEdad();
