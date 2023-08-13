5491486412f4e9662c012f0a07513d7e293f4b72
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-08-13 03:06:17 -0300

    main
    ejercicios de jon mircha 5-8

--- jon-mircha/ejercicios01/ejercicioString.js
+++ jon-mircha/ejercicios01/ejercicioString.js
@@ -53,3 +53,68 @@ function repetirString(cadena,repeticion){
 }
 console.log(repetirString("hola Mundo",4));
 // console.log("hola")
+
+/**
+ *  5) Programa una función que invierta las palabras de una cadena de texto, 
+ *      pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+ */
+function invierteLaCadenaDeTexto(cadena){
+    let aux = "";
+    let ultimaPosicion = cadena.length;
+    for(let i = 0; i<cadena.length;i++){
+
+        aux += cadena.charAt((ultimaPosicion-1)-i);
+        
+    }
+    return aux;
+}
+console.log(invierteLaCadenaDeTexto("hola Mundo"));
+
+/**
+ * 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, 
+        pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+*/
+function cuentaPalabras(cadena, palabraABuscar){
+    const arrayPalabra = cadena.split(" ")
+    console.log(arrayPalabra);
+    let contador = 0;
+    for(let i = 0; i<arrayPalabra.length;i++){
+        if(arrayPalabra[i] === palabraABuscar){
+            contador++;
+        }
+    }
+    return contador;
+}
+console.log(cuentaPalabras("este es un array de un un un una palabras","un"))
+
+/**
+    7) Programa una función que valide si una palabra o frase dada, es un palíndromo 
+        (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+*/
+function esPalindromo (cadena){
+    cadena = cadena.toLowerCase();
+    let posicionFInal = cadena.length-1;
+    let i = 0;
+    let cortaFuego = true;
+    while(i<cadena.length&&cortaFuego===true){
+        if (cadena[i] === cadena[posicionFInal-i]){
+            i++;
+            cortaFuego = true;
+        }
+        else{
+            cortaFuego = false;
+        }
+    }
+    return cortaFuego;
+}
+console.log(esPalindromo("SalaSa"))
+/**
+    8) Programa una función que elimine cierto patrón de caracteres de un texto dado, 
+        pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+ */
+function eliminaPatronEnCadenaDeTexto(cadena, patron){
+    cadena = cadena.replaceAll(patron,"");
+    return cadena;
+}
+console.log(eliminaPatronEnCadenaDeTexto("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz"));
+
